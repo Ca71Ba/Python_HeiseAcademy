9c8a8daa6d4b7c2ab65cf5715c586612374917ba
Carsten <carsten.bauer@gmail.com>
2023-05-29 23:31:35 +0200

    Initial Load

---
+++ HelloWorld.py
@@ -0,0 +1,22 @@
+first_name = "CaRsTen"
+last_name = "Bauer"
+
+x = 10         # integer
+y = 4
+z = 100.99      # float
+
+# a,b,c = 1, 2, 4
+
+a, b = 1.25, 5
+result = a*b
+
+print(f"Ergebnis:", result)
+print(f"Ergebnis: {result}")
+
+
+
+# print(f"Hello {first_name} {last_name}!")
+# print(f"Hello {first_name.title()} {last_name.upper()}!")
+# print("Hello World!")
+# print (x%y) # Modulo-Berechnung
+# print (a+b+c)
