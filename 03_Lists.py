09a260e69372a920374448aeeb8f39a31ca12072
Carsten <carsten.bauer@gmail.com>
2023-06-03 09:08:46 +0200

    sorting added

--- 03_Lists.py
+++ 03_Lists.py
@@ -1,9 +1,12 @@
-animals =["Affe", "Giraffe", "Nashorn", "Gans"]
-zahlen =[1,2,3,4,5]
+animals = ["Giraffe", "Affe", "Zebra", "Nashorn", "Gans"]
+zahlen = [1,2,3,4,5]
 
 print(animals[1])
-print(animals[3])
+# print(animals[3])
 print(animals)
+
 print(zahlen[0])
 print(zahlen)
 
+animals.sort()
+print(f"sorted: {animals}")
